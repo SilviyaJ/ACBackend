bf4e83ba630cfe3b3210e4adeb807149211f003d
SilviyaJ <silviyajoshua22@gmail.com>
2022-07-08 15:02:39 +0530

    ac backend

---
+++ src/main/java/com/example/Acproject/AcprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Acproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Acproject/AcprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Acproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
